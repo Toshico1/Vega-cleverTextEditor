f93256c13a47d60f77d802b74bd93f37c2eb07f3
Lex <43500461+Toshico1@users.noreply.github.com>
2019-05-28 09:11:21 +0800

    UPD
    Добавлена реализация сохранения, импорта и создания файла
    Добавлено изменение цвета фона

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,57 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_button_import_clicked()
+{
+    QString file_path = QFileDialog::getOpenFileName(0, "Open file", "", "*.txt *.cpp *.h *.py");
+    QFile file(file_path);
+    file.open(QIODevice::ReadWrite);
+    QString file_data = file.readAll();
+    ui->plainTextEdit->setPlainText(file_data);
+
+}
+
+void MainWindow::on_button_new_clicked()
+{
+    QString str = QFileDialog::getExistingDirectory(0, "Path of new file", "");
+    QFile file("some.txt");
+    file.open(QIODevice::WriteOnly);
+    file.close();
+
+}
+
+void MainWindow::on_button_save_clicked()
+{
+    QString data = ui->plainTextEdit->toPlainText();
+    QString path = QFileDialog::getExistingDirectory(0, "Path of new file", "");
+    QFile file("some.txt");
+    file.open(QIODevice::WriteOnly);
+    QTextStream steam(&file);
+    steam << data;
+    file.close();
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+
+    if(ui->radio_black->isChecked() == true){
+            QPalette p = ui->plainTextEdit->palette();
+            p.setColor(QPalette::Active, QPalette::Base, Qt::black);
+            ui->plainTextEdit->setPalette(p);
+    }else if (ui->radio_white->isChecked() == true) {
+        QPalette p = ui->plainTextEdit->palette();
+        p.setColor(QPalette::Active, QPalette::Base, Qt::white);
+        ui->plainTextEdit->setPalette(p);
+    }else if (ui->radio_green->isChecked() == true) {
+        QPalette p = ui->plainTextEdit->palette();
+        p.setColor(QPalette::Active, QPalette::Base, Qt::green);
+        ui->plainTextEdit->setPalette(p);
+    }else if (ui->radio_grey->isChecked() == true) {
+        QPalette p = ui->plainTextEdit->palette();
+        p.setColor(QPalette::Active, QPalette::Base, Qt::gray);
+        ui->plainTextEdit->setPalette(p);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
+#include <QPalette>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +19,19 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+
+    void on_button_import_clicked();
+
+    void on_button_new_clicked();
+
+    void on_button_save_clicked();
+
+    void on_pushButton_6_clicked();
+
+signals:
+    void send_Data_file(const QStringList);
 
 private:
     Ui::MainWindow *ui;
